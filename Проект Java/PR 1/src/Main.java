b36131caf9842087f127feee9ea089d922f8fbda
Ambrosov Danyil <166511703+F1xaze11@users.noreply.github.com>
2025-03-24 12:38:48 +0200

    Завдання 1

---
+++ Проект Java/PR 1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arguments = {"ППФК", "Практика", "2025"};
+        System.out.println("Аргументи командного рядка:");
+        for (int i = 0; i < arguments.length; i++) {
+            System.out.println("Аргумент " + i + ": " + arguments[i]);
+        }
+    }
+}
